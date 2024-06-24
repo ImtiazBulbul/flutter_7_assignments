fb1a92d10753ad15e1c584846c880135cb1a1a50
ImtiazBulbul <imtiazbulbulsunny563@gmail.com>
2024-06-24 23:13:04 +0600

    live_test.dart file edited

--- lib/live_test/live_test.dart
+++ lib/live_test/live_test.dart
@@ -1,6 +1,28 @@
-main(){
-  int x=20;
-  int y=10;
-  print(x/y);
-  print('hello');
+class Media {
+  void play() {
+    print("Playing media...");
+  }
+}
+
+class Song extends Media {
+  final String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+void main() {
+  // Create an instance of Media
+  Media media = Media();
+
+  // Create an instance of Song with an artist name
+  Song song = Song("The Beatles");
+
+  // Call play() methods
+  media.play();
+  song.play();
 }
